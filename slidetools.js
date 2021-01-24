792415d5f4fdaf1f34c52bf3617bda768a2dcc26
danstone987 <77899353+danstone987@users.noreply.github.com>
2021-01-23 20:28:51 -0500

    Swipe attempt

--- slidetools.js
+++ slidetools.js
@@ -1,5 +1,52 @@
 "use strict";
 
+function detectswipe(el,func) {
+  swipe_det = new Object();
+  swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+  var min_x = 30;  //min x swipe for horizontal swipe
+  var max_x = 30;  //max x difference for vertical swipe
+  var min_y = 50;  //min y swipe for vertical swipe
+  var max_y = 60;  //max y difference for horizontal swipe
+  var direc = "";
+  ele = document.getElementById(el);
+  ele.addEventListener('touchstart',function(e){
+    var t = e.touches[0];
+    swipe_det.sX = t.screenX; 
+    swipe_det.sY = t.screenY;
+  },false);
+  ele.addEventListener('touchmove',function(e){
+    e.preventDefault();
+    var t = e.touches[0];
+    swipe_det.eX = t.screenX; 
+    swipe_det.eY = t.screenY;    
+  },false);
+  ele.addEventListener('touchend',function(e){
+    //horizontal detection
+    if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+      if(swipe_det.eX > swipe_det.sX) 
+	  {
+        imageNumber = getNextImageNumber(categoryNumber, imageNumber);
+        window.location.href = buildURL("slide.html", imageNumber, categoryNumber);
+	  }
+      else {
+        imageNumber = getPreviousImageNumber(categoryNumber, imageNumber);
+        window.location.href = buildURL("slide.html", imageNumber, categoryNumber);
+	  }
+    }
+    //vertical detection
+    else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+      if(swipe_det.eY > swipe_det.sY) 
+	  {
+	    // down
+	  }
+      else 
+	  {
+	    // up
+	  }
+    }
+    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+  },false);  
+}
 
 document.addEventListener('keydown', function(event) 
 {
