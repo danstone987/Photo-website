12ce7ad0dd2476626a0afed45d984ca6bdbfe325
danstone987 <77899353+danstone987@users.noreply.github.com>
2021-01-23 21:19:33 -0500

    Add files via upload

--- slidetools.js
+++ slidetools.js
@@ -1,54 +1,57 @@
 "use strict";
 
-function detectswipe(el) {
-  swipe_det = new Object();
+function detectswipe() {
+  var swipe_det = new Object();
   swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
   var min_x = 30;  //min x swipe for horizontal swipe
   var max_x = 30;  //max x difference for vertical swipe
   var min_y = 50;  //min y swipe for vertical swipe
   var max_y = 60;  //max y difference for horizontal swipe
   var direc = "";
-  ele = document.getElementById(el);
-  ele.addEventListener('touchstart',function(e){
-    var t = e.touches[0];
-    swipe_det.sX = t.screenX; 
-    swipe_det.sY = t.screenY;
-  },false);
-  ele.addEventListener('touchmove',function(e){
-    e.preventDefault();
-    var t = e.touches[0];
-    swipe_det.eX = t.screenX; 
-    swipe_det.eY = t.screenY;    
-  },false);
-  ele.addEventListener('touchend',function(e){
-    //horizontal detection
-    if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
-      if(swipe_det.eX > swipe_det.sX) 
-	  {
-        imageNumber = getNextImageNumber(categoryNumber, imageNumber);
-        window.location.href = buildURL("slide.html", imageNumber, categoryNumber);
-	  }
-      else {
-        imageNumber = getPreviousImageNumber(categoryNumber, imageNumber);
-        window.location.href = buildURL("slide.html", imageNumber, categoryNumber);
-	  }
-    }
-    //vertical detection
-    else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
-      if(swipe_det.eY > swipe_det.sY) 
-	  {
-	    // down
-	  }
-      else 
-	  {
-	    // up
-	  }
-    }
-    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
-  },false);  
+  var ele = document.getElementById("slideImage");
+  if (ele !== null)
+  {
+	  ele.addEventListener('touchstart',function(e){
+		var t = e.touches[0];
+		swipe_det.sX = t.screenX; 
+		swipe_det.sY = t.screenY;
+	  },false);
+	  ele.addEventListener('touchmove',function(e){
+		e.preventDefault();
+		var t = e.touches[0];
+		swipe_det.eX = t.screenX; 
+		swipe_det.eY = t.screenY;    
+	  },false);
+	  ele.addEventListener('touchend',function(e){
+		//horizontal detection
+		if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+		  if(swipe_det.eX > swipe_det.sX) 
+		  {
+			imageNumber = getNextImageNumber(categoryNumber, imageNumber);
+			window.location.href = buildURL("slide.html", imageNumber, categoryNumber);
+		  }
+		  else {
+			imageNumber = getPreviousImageNumber(categoryNumber, imageNumber);
+			window.location.href = buildURL("slide.html", imageNumber, categoryNumber);
+		  }
+		}
+		//vertical detection
+		else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+		  if(swipe_det.eY > swipe_det.sY) 
+		  {
+			// down
+		  }
+		  else 
+		  {
+			// up
+		  }
+		}
+		swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+	  },false);  
+  }
 }
 
-detectswipe('slideImage');
+
 
 document.addEventListener('keydown', function(event) 
 {
@@ -72,6 +75,7 @@ function setSize()
 {
     var slideImage = document.getElementById("slideImage");
     resizeImg(slideImage, 50, 250);
+	detectswipe();
 }
 
 window.onresize = function () 
